59b033c061674f63fda75032d95f93c6249aacfb
David Fariña <info@davidfarina.com>
2022-06-29 21:50:33 +0200

    Better list implementation

--- src/components/FieldList.tsx
+++ src/components/FieldList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { deepGet, deepSet, FormRefObject } from "@ezform/core";
 
 interface FieldListProps {
@@ -15,30 +15,39 @@ interface FieldListProps {
 
 export const FieldList = (props: FieldListProps) => {
 	const { form, name, renderRow, validateOnChange = false } = props;
+	const [length, setLength] = useState<number>(null);
+
+	useEffect(() => {
+		if (!length) {
+			let arr = deepGet(form.getFields(), name) || [];
+			setLength(arr?.length);
+		}
+	}, [form.getFields()]);
 
 	const add = () => () => {
 		form.setFields((fields) => {
-			const arr = deepGet(fields, name) || [];
+			let arr = deepGet(fields, name) || [];
 			arr.push({});
 			const newObj = { ...fields };
 			deepSet(newObj, name, arr);
+			setLength((p) => p+1);
 			return newObj;
 		}, validateOnChange);
 	};
 
 	const remove = (i: number) => () => {
 		form.setFields((fields) => {
-			const arr = deepGet(fields, name);
-			arr?.splice(i, 1);
+			let arr = deepGet(fields, name) || [];
+			arr.splice(i, 1);
+			setLength((p) => p-1);
 			return fields;
 		}, validateOnChange);
 	};
 
 	const render = () => {
-		const arr = form.getFields()[name] || [];
 		const rows = [];
-		for (let index = 0; index <= arr.length; index++) {
-			rows.push(renderRow({ add, remove, index, total: arr.length }));
+		for (let index = 0; index <= (length || 0); index++) {
+			rows.push(renderRow({ add, remove, index, total: length }));
 		}
 		return rows;
 	};
